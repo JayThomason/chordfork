f7c5b6dc481a168129b9a80bd0969d63e0255855
JayThomason <jayt92@stanford.edu>
2013-02-26 19:53:00 +0000

    added functionality to update chords on the screen when next button is pressed, added chord class.

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,8 +1,30 @@
+/* 
+ * Object which represents a chord. Includes the note, type, and modifiers.
+ *
+ * For example, for the chord C#Maj7, the note is C#, the type is Maj, and
+ * the modifier is 7.
+ * */
 
-/*
- * Callback functions for chord buttons used in /create.
- */
+function Chord (note, noteMod, type, mod) {
+  this.note = note;
+  this.noteMod = noteMod;
+  this.type = type;
+  this.mod = mod;
+}
 
+/* Prints the current chord to the current-chord div on the create page. */
+Chord.prototype.print = function () {
+  this.note = this.note == null ? "" : this.note;
+  this.noteMod = this.noteMod == null ? "" : this.noteMod;
+  this.type = this.type == null ? "" : this.type;
+  this.mod = this.mod == null ? "" : this.mod;
+  $("#current-chord").text("current: " + this.note + this.noteMod + this.type + this.mod);
+}
+
+chord = new Chord (null, null, null, null);
+chords = [];
+
+/* Helper function which determines valid mod notes. */
 function isValidModNote(ch) {
   var notes = ["1", "3", "5", "6", "7", "9", "11", "13"];
   for (var i = 0; i < notes.length; i++) {
@@ -13,56 +35,70 @@ function isValidModNote(ch) {
   return false;
 }
 
-function printChord() {
-  noteMod = typeof(noteMod) === "undefined" ? "" : noteMod;
-  type = typeof(type) === "undefined" ? "" : type;
-  mod = typeof(mod) === "undefined" ? "" : mod;
-  alert ("Current chord is now: " + note + noteMod + type + mod);
-}
+/*
+ * Callback functions for chord buttons used in /create.
+ */
 
 $(".chord-note").click(function () {
-  note = "";
-  noteMod = "";
-  type = "";
-  mod = "";
-  note = $(this).text();
-  printChord();
+  chord.note = "";
+  chord.noteMod = "";
+  chord.type = "";
+  chord.mod = "";
+  chord.note = $(this).text();
+  chord.print ();
 });
 
 $(".chord-note-mod").click(function () {
-  if (typeof(note) === "undefined" || note == "")
+  if (chord.note == null || chord.note == "")
     return;
-  if (typeof(mod) === "undefined" || mod == "") {
-    if (noteMod == $(this).text())
-      noteMod = "";
+  if (chord.mod == null || chord.mod == "") {
+    if (chord.noteMod == $(this).text())
+      chord.noteMod = "";
     else
-      noteMod = $(this).text();
+      chord.noteMod = $(this).text();
   }
   else {
-    if (isValidModNote(mod[mod.length - 1]))
-      mod += $(this).text();
+    if (isValidModNote(chord.mod[chord.mod.length - 1]))
+      chord.mod += $(this).text();
   }
-  printChord();
+  chord.print ();
 });
 
 $(".chord-tri").click(function () {
-  if (typeof(note) === "undefined" || note == "")
+  if (chord.note == null || chord.note == "")
     return;
-  type = $(this).text();
-  printChord();
+  chord.type = $(this).text();
+  chord.print ();
 });
 
 $(".chord-mod").click(function () {
-  if (typeof(note) === "undefined" || note == "")
+  if (chord.note == null || chord.note == "")
     return;
-  mod = typeof(mod) === "undefined" ? "" : mod;
-  mod += $(this).text();
-  printChord();
+  chord.mod = typeof(chord.mod) === "undefined" ? "" : chord.mod;
+  chord.mod += $(this).text();
+  chord.print ();
 });
 
 $("#clear").click(function () {
-  note = "";
-  noteMod = "";
-  type = "";
-  mod = "";
+  chords.push (new Chord (chord.note, chord.noteMod, chord.type, chord.mod));
+  chord.note = null;
+  chord.noteMod = null;
+  chord.type = null;
+  chord.mod = null;
+  chord.print ();
+});
+
+$("#next-chord").click (function () {
+  if (chord.note == null || chord.note == "")
+    return;
+  chords.push (new Chord (chord.note, chord.noteMod, chord.type, chord.mod));
+  chord.note = null;
+  chord.noteMod = null;
+  chord.type = null;
+  chord.mod = null;
+  var index = chords.length - 1;
+  var lastChord = chords[index];
+  $(".display-chords").append ("<strong id='test" + index + "'>" 
+      + lastChord.note + lastChord.noteMod + lastChord.type + lastChord.mod + '</strong>  ');
+  chord.print ();
 });
