593a18271dad382f7c99f1c48d187daec4ddc1aa
JayThomason <jayt92@stanford.edu>
2013-03-08 07:22:00 +0000

    commented user.js, removed comments from index.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,25 @@
+/**
+ * Controller: User
+ * -------------------------------------------------------
+ *  Actions undertaken by users which interact with their
+ *  own accounts, such as logging in and signing up.
+ */
+
+/* Require sequelize module */
 var db = require ('../sequelize-singleton');
 var User = db.model ('user');
 
+/* Logs a user in by setting session variables and
+ * redirecting to /create. This should later be changed
+ * to the home page.
+ */
 function session_login (req, res, user) {
   req.session.logged_in = true;
   req.session.username = user.name;
   res.redirect ('/create');
 }
 
+/* POST /user/login */
 exports.login =  function (req, res) {
   User.find({ 
     where: {
@@ -20,9 +33,13 @@ exports.login =  function (req, res) {
     else {
       session_login (req, res, user);
     }
+  }).error (function () {
+    console.log ('Failed to find user.');
+    res.send ('Failed to find user.');
   });
 }
 
+/* POST user/create */
 exports.create = function (req, res) {
   var username = req.body.create_username;
   var pw = req.body.create_password;
